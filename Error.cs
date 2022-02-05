fb54e02b7f631aa85926e9ff67a4270e9d786715
Esdras-Henrique <63551459+Esdras-Henrique@users.noreply.github.com>
2022-02-05 11:26:57 -0300

    First Commit

---
+++ Error.cs
@@ -0,0 +1,14 @@
+namespace rror
+{
+    
+    class Error
+    {
+        public int numero = 1;
+
+        public Error(int a)
+        {
+            numero = 4;
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        Error a1 = new Error(1);
+        
+    }
+}
